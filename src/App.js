508b765f7bd51de6b2499d625975526333ba98de
RoxtonRoyal <152863958+RoxtonRoyal@users.noreply.github.com>
2024-03-04 23:04:56 +0530

    navigation commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import OurSevices from "./OurSevices";
+
 function App() {
   return (
-    <div className="App">
-      <center><h1>Welcome Ali Roxton</h1></center>
-    </div>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+  {/* Navigation links */}
+  
+
+  {/* Components */}
+  <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+    <Routes>
+      <Route  path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/ourservices" element={<OurSevices />} />
+    </Routes>
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+    <Link style={{ margin: 8 }} to="/home">Home</Link>
+    <Link style={{ margin: 8 }} to="/about">About Us</Link>
+    <Link style={{ margin: '8px 16px 8px 8px' }} to="/ourservices">Our Services</Link>
+
+  </div>
+</div>
   );
 }
 
